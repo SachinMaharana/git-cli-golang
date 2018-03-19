25ae42bcb73e1639c7d03adb7f15a7d3712f3838
Sachin <sachin.nicky.com>
2018-03-20 02:08:11 +0530

    Updated code

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -14,44 +15,90 @@ import (
 
 func init() {
 	fmt.Println(`Starting...`)
+	if os.Getenv("GITHUB_AUTH_TOKEN") == "" {
+		log.Fatal("Unauthorized: No token present.")
+		os.Exit(1)
+	}
 }
 
 var (
 	name        = flag.String("name", "", "Name of Repo to be created....")
-	description = flag.String("description", "", "Description of created repo.")
+	description = flag.String("desc", "", "Description of created repo....")
 )
+
+func createRepo(token string, name *string, description *string) (repo *github.Repository, err error) {
+	ctx := context.Background()
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tokenContex := oauth2.NewClient(ctx, tokenSource)
+	client := github.NewClient(tokenContex)
+	r := &github.Repository{Name: name, Description: description}
+	repo, _, err = client.Repositories.Create(ctx, "", r)
+	return repo, err
+}
+
+func cloneRepo(repo *github.Repository) (err error) {
+	repoName := repo.GetName()
+	var cloneError error
+	if repoName != "" {
+		url := fmt.Sprintf("https://github.com/SachinMaharana/%s", repoName)
+		cmd := exec.Command("git", "clone", url)
+		err := cmd.Run()
+		cloneError = err
+	}
+	return cloneError
+}
 
 func main() {
 	flag.Parse()
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
-	if token == "" {
-		log.Fatal("Unauthorized: No token present.")
-	}
 
 	if *name == "" {
 		log.Fatal("No name: New Repos must be given name")
 	}
 
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-	r := &github.Repository{Name: name, Description: description}
-	repo, _, err := client.Repositories.Create(ctx, "", r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(`Created: `, repo.GetName())
-	if repo.GetName() != "" {
-		url := fmt.Sprintf("https://github.com/SachinMaharana/%s", repo.GetName())
-		cmd := exec.Command("git", "clone", url)
-		out, err := cmd.Output()
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(string(out))
-	}
-	fmt.Println(`Ready....Start Coding....`)
+	repo, err := createRepo(token, name, description)
+
+	check(err)
+
+	fmt.Println(`Repo Created:`, repo.GetName())
+
+	cloneError := cloneRepo(repo)
+
+	check(cloneError)
+
+	log.Println("Git Clone done for", repo.GetName())
+
+	createFile(repo.GetName(), "README.md")
+
+	content := fmt.Sprintf("#%s", repo.GetName())
+
+	writeFile(repo.GetName(), "README.md", content)
+
+	log.Println(`Ready....Start Coding....`)
 
 	// https://github.com/SachinMaharana/test_repo_working.git
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func createFile(path string, fileName string) {
+	file, err := os.Create(filepath.Join(path, fileName))
+	check(err)
+	log.Println(`File Created.`)
+	defer file.Close()
+}
+
+func writeFile(newPath string, fileName string, content string) {
+	file, err := os.OpenFile(filepath.Join(newPath, fileName), os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0666)
+	check(err)
+	byteSlice := []byte(content)
+	_, errr := file.Write(byteSlice)
+	check(errr)
+	defer file.Close()
 }
